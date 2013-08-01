8afed85b51d2e56af2334e81b70f95b680c5ba2b
Galina Slavova <galina@crafity.com>
2013-08-01 22:27:49 +0200

    Added an ansi submodule dependency

--- main.js
+++ main.js
@@ -10,6 +10,13 @@
  */
 
 /**
+ * Module dependencies.
+ */
+var ansi = require("ansi")
+	, ansiCursor = ansi(process.stdout)
+	;
+
+/**
  * Framework name.
  */
 exports.fullname = 'crafity-logging';
@@ -27,58 +34,140 @@ exports.version = '0.1.1';
  * @return {*} A new logger instance
  */
 exports.create = function (name, category, config) {
+exports.create = function (name, category, config) { // 
+	/**
+	 * Module dependencies.
+	 */
+	var log4js = require('crafity-log4js')
+		, fs = require('crafity-filesystem')
+		;
 
-  /**
-   * Module dependencies.
-   */
+	/**
+	 * Organize the parameters
+	 */
+	if (name === undefined && category === undefined && config === undefined) {
+		name = undefined;
+		category = undefined;
+		config = {};
 
-  var log4js = require('crafity-log4js')
-    , fs = require('crafity-filesystem');
+	} else if (typeof name === 'string' && typeof category === 'object' && config === undefined) {
+		config = category;
+		category = undefined;
 
-  /**
-   * Organize the parameters
-   */
-  if (name === undefined && category === undefined && config === undefined) {
-    name = undefined;
-    category = undefined;
-    config = {};
-  } else if (typeof name === 'string' && typeof category === 'object' && config === undefined) {
-    config = category;
-    category = undefined;
-  } else if (typeof name === 'object' && category === undefined && config === undefined) {
-    config = name;
-    name = config.name;
-    category = config.category;
-  }
+	} else if (typeof name === 'object' && category === undefined && config === undefined) {
+		config = name;
+		name = config.name;
+		category = config.category;
+	}
 
-  /**
-   * Variable declatations
-   */
-  var logger;
+	
+	/**
+	 * Variable declarations
+	 */
+	var logger;
 
-  /**
-   * Initialize the appenders
-   */
-  config.appenders = (config.appenders || []).map(function (appender) {
-    appender.name = name || appender.name;
-    appender.category = category || appender.category;
-    appender.filename = appender.filename ||
-      (appender.path && fs.combine(process.cwd(), appender.path, appender.name + ".log"));
-    return appender;
-  });
+	/**
+	 * Initialize the appenders
+	 */
+	config.appenders = (config.appenders || []).map(function (appender) {
+		appender.name = name || appender.name;
+		appender.category = category || appender.category;
+		appender.filename = appender.filename ||
+			(appender.path && fs.combine(process.cwd(), appender.path, appender.name + ".log"));
+		return appender;
+	});
 
-  /**
-   * Configure log4js
-   */
-  log4js.configure(config, { keep: config.keep !== false });
+//	console.log("SEGA #1");
+	/**
+	 * Configure log4js, which is internally used.
+	 * 
+	 * NB! This method may return a logger object, so the code below is then unreached.
+	 */
+	logger = log4js.configure(config, { keep: config.keep !== false });
 
-  /**
-   * Get the logger and return it
-   */
-  logger = log4js.getLogger(name);
-  logger.setLevel('ALL');
-  logger.name = name;
-  return logger;
+	
+//	console.log("SEGA #2");
+
+//	console.log("\n\n\n\n TEST UNREACHABLE when config = { keep: false } .. SEGA!!!!!, log4js.logger = ", logger);
+	
+	/**
+	 * Get the logger and return it
+	 */
+	logger = log4js.getLogger(name);
+	logger.setLevel('ALL');
+	logger.name = name;
+
+	logger.http = {};
+	logger.http.logRequestResponse = function (req, res, err) {
+		var crafityColours = {
+			orange: "#E38418",
+			red: "#C21705",
+			brown: "#483432",
+			lightGray: "#525252",
+			gray: "#a1a1a1",
+			blue: "#7cb6e9",
+			white: "#D8D7D7"
+		};
+
+		ansiCursor
+			.hex(crafityColours.brown).write("Client:")
+			.fg.reset()
+			.hex(crafityColours.white)
+			.write(req.client.remoteAddress + ":" + req.client.remotePort)
+			.fg.reset()
+
+			.hex(crafityColours.brown).write(", Method: ")
+			.fg.reset()
+			.hex(crafityColours.orange)
+			.write(req.method)
+			.fg.reset()
+
+			.hex(crafityColours.brown).write(", Status: ")
+			.fg.reset()
+			.hex(crafityColours.orange)
+			.write(res.statusCode || "?")
+			.fg.reset()
+	
+			.hex(crafityColours.brown).write(", Url: ")
+			.fg.reset()
+			.hex(crafityColours.orange)
+			.write(req.url)
+			.fg.reset()
+			
+			.hex(crafityColours.brown).write(", Host: ")
+			.fg.reset()
+			.hex(crafityColours.white)
+			.write(req.headers.host)
+			.fg.reset()
+
+			.hex(crafityColours.brown).write((req.headers && req.headers.referer) ? ", Referer: " : "")
+			.fg.reset()
+			.hex(crafityColours.white)
+			.write((req.headers && req.headers.referer) ? req.headers.referer : "")
+			.fg.reset()
+
+			.hex(crafityColours.brown).write(", UA: ")
+			.fg.reset()
+			.hex(crafityColours.white)
+			.write(req.headers["user-agent"])
+			.fg.reset()
+
+			.hex(crafityColours.brown).write(", Date: ")
+			.fg.reset()
+			.hex(crafityColours.blue)
+			.write(new Date().toString())
+			.fg.reset()
+
+			.hex(crafityColours.red).write(err ? "\nERROR: " : "")
+			.fg.reset()
+			.hex(crafityColours.orange)
+			.write(err ? (err.toString() + (err.stack ? err.stack + "\n" : "")) : "")
+			.fg.reset()
+
+			.reset();
+	};
+
+	return logger;
 };
 
 //(function () {
