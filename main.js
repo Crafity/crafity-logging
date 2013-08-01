1a4cce5986b24cc3a0a0390a8ef16c4eabc72dd3
Galina Slavova <galina@crafity.com>
2013-08-01 22:28:06 +0200

    Deleted commented code

--- main.js
+++ main.js
@@ -170,37 +170,3 @@ exports.create = function (name, category, config) { //
 	return logger;
 };
 
-//(function () {
-//	"use strict";
-//	
-//	return;
-//
-////console.log("log4js", log4js);
-//
-////log4js.configure("logging.json", {});
-//	log4js.configure(undefined, {
-//		"appenders": [
-//			{
-//				"type": "file",
-//				"filename": "logs/test.log",
-//				"name": "cheese",
-//				"maxLogSize": 1024,
-//				"backups": 10,
-//				"pollInterval": 15
-//			}
-//		]
-//	});
-//
-//	log4js.addAppender(log4js.consoleAppender());
-////log4js.addAppender(log4js.fileAppender('logs/cheese.log'), 'cheese');
-//
-//	var logger = log4js.getLogger('cheese');
-//	logger.setLevel('ALL');
-//
-//	logger.trace('Entering cheese testing');
-//	logger.debug('Got cheese.');
-//	logger.info('Cheese is Gouda.');
-//	logger.warn('Cheese is quite smelly.');
-//	logger.error('Cheese is too ripe!');
-//	logger.fatal('Cheese was breeding ground for listeria.');
-//}());
