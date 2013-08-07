f688afd2123ddbb60252c4d795417f248f0bbaf1
Bart Riemens <briemens@crafity.com>
2013-08-07 21:48:11 +0200

    Merged changes from master

--- main.js
+++ main.js
@@ -3,11 +3,19 @@
 
 /*!
  * crafity-logging - Crafity's logging abstraction
- * Copyright(c) 2011-2012 Crafity
- * Copyright(c) 2011-2012 Bart Riemens
- * Copyright(c) 2011-2012 Galina Slavova
+ * Copyright(c) 2011-2013 Crafity
+ * Copyright(c) 2011-2013 Bart Riemens
+ * Copyright(c) 2011-2013 Galina Slavova
  * MIT Licensed
  */
+
+/**
+ * Module dependencies.
+ */
+var ansi = require("ansi")
+	, ansiCursor = ansi(process.stdout)
+	, moment = require("moment")
+	;
 
 /**
  * Framework name.
@@ -17,101 +25,141 @@ exports.fullname = 'crafity-logging';
 /**
  * Framework version.
  */
-exports.version = '0.1.1';
+exports.version = '0.1.3';
 
 /**
- * Create a new logger instance
+ * Create a new logger instance.
+ *
  * @param name The name of the logger
  * @param category The category of the logger
- * @param config More configuration opetions
+ * @param config More configuration options
  * @return {*} A new logger instance
  */
 exports.create = function (name, category, config) {
+	/**
+	 * Module dependencies.
+	 */
+	var log4js = require('crafity-log4js')
+		, fs = require('crafity-filesystem')
+		;
 
-  /**
-   * Module dependencies.
-   */
+	/**
+	 * Organize the parameters
+	 */
+	if (name === undefined && category === undefined && config === undefined) {
+		name = undefined;
+		category = undefined;
+		config = {};
 
-  var log4js = require('crafity-log4js')
-    , fs = require('crafity-filesystem');
+	} else if (typeof name === 'string' && typeof category === 'object' && config === undefined) {
+		config = category;
+		category = undefined;
 
-  /**
-   * Organize the parameters
-   */
-  if (name === undefined && category === undefined && config === undefined) {
-    name = undefined;
-    category = undefined;
-    config = {};
-  } else if (typeof name === 'string' && typeof category === 'object' && config === undefined) {
-    config = category;
-    category = undefined;
-  } else if (typeof name === 'object' && category === undefined && config === undefined) {
-    config = name;
-    name = config.name;
-    category = config.category;
-  }
+	} else if (typeof name === 'object' && category === undefined && config === undefined) {
+		config = name;
+		name = config.name;
+		category = config.category;
+	}
 
-  /**
-   * Variable declatations
-   */
-  var logger;
+	/**
+	 * Variable declarations.
+	 */
+	var logger;
 
-  /**
-   * Initialize the appenders
-   */
-  config.appenders = (config.appenders || []).map(function (appender) {
-    appender.name = name || appender.name;
-    appender.category = category || appender.category;
-    appender.filename = appender.filename ||
-      (appender.path && fs.combine(process.cwd(), appender.path, appender.name + ".log"));
-    return appender;
-  });
+	/**
+	 * Initialize the appenders.
+	 */
+	config.appenders = (config.appenders || []).map(function (appender) {
+		appender.name = name || appender.name;
+		appender.category = category || appender.category;
+		appender.filename = appender.filename ||
+			(appender.path && fs.combine(process.cwd(), appender.path, appender.name + ".log"));
+		return appender;
+	});
 
-  /**
-   * Configure log4js
-   */
-  log4js.configure(config, { keep: config.keep !== false });
+	/**
+	 * Configure log4js, which is internally used.
+	 */
+	log4js.configure(config, { keep: config.keep !== false });
 
-  /**
-   * Get the logger and return it
-   */
-  logger = log4js.getLogger(name);
-  logger.setLevel('ALL');
-  logger.name = name;
-  return logger;
+	/**
+	 * Get the logger and return it
+	 */
+	logger = log4js.getLogger(name);
+	logger.setLevel('ALL');
+	logger.name = name ||  "";
+	logger.appenders = log4js.appenders;
+
+	logger.http = {};
+	logger.http.logRequestResponse = function (req, res, err) {
+		var crafityColours = {
+			orange: "#E38418",
+			red: "#C21705",
+			brown: "#483432",
+			lightGray: "#525252",
+			gray: "#a1a1a1",
+			blue: "#7cb6e9",
+			white: "#D8D7D7"
+		};
+
+		ansiCursor
+			.hex(crafityColours.blue)
+			.write(moment().format("YYYYMMDD, HH:mm:ss.SSS ZZ")) //source: http://momentjs.com/docs/
+			.fg.reset()
+
+			.hex(crafityColours.brown).write(" - Client:")
+			.fg.reset()
+			.hex(crafityColours.white)
+			.write(req.client.remoteAddress + ":" + req.client.remotePort)
+			.fg.reset()
+
+			.hex(crafityColours.brown).write(", Method: ")
+			.fg.reset()
+			.hex(crafityColours.orange)
+			.write(req.method)
+			.fg.reset()
+
+			.hex(crafityColours.brown).write(", Status: ")
+			.fg.reset()
+			.hex(crafityColours.orange)
+			.write(res.statusCode ? res.statusCode.toString() : "?")
+			.fg.reset()
+
+			.hex(crafityColours.brown).write(", Url: ")
+			.fg.reset()
+			.hex(crafityColours.orange)
+			.write(req.url)
+			.fg.reset()
+
+			.hex(crafityColours.brown).write(", Host: ")
+			.fg.reset()
+			.hex(crafityColours.white)
+			.write(req.headers.host)
+			.fg.reset()
+
+			.hex(crafityColours.brown).write((req.headers && req.headers.referer) ? ", Referer: " : "")
+			.fg.reset()
+			.hex(crafityColours.white)
+			.write((req.headers && req.headers.referer) ? req.headers.referer : "")
+			.fg.reset()
+
+			.hex(crafityColours.brown).write(", UA: ")
+			.fg.reset()
+			.hex(crafityColours.white)
+			.write(req.headers["user-agent"])
+			.write("\n")
+			.fg.reset()
+
+			.hex(crafityColours.red).write(err ? "\nERROR: " : "")
+			.fg.reset()
+			.hex(crafityColours.orange)
+			.write(err ? (err.toString() + (err.stack ? err.stack + "\n" : "")) : "")
+			.fg.reset()
+
+			.reset();
+	};
+
+	return logger;
 };
 
-//(function () {
-//	"use strict";
-//	
-//	return;
-//
-////console.log("log4js", log4js);
-//
-////log4js.configure("logging.json", {});
-//	log4js.configure(undefined, {
-//		"appenders": [
-//			{
-//				"type": "file",
-//				"filename": "logs/test.log",
-//				"name": "cheese",
-//				"maxLogSize": 1024,
-//				"backups": 10,
-//				"pollInterval": 15
-//			}
-//		]
-//	});
-//
-//	log4js.addAppender(log4js.consoleAppender());
-////log4js.addAppender(log4js.fileAppender('logs/cheese.log'), 'cheese');
-//
-//	var logger = log4js.getLogger('cheese');
-//	logger.setLevel('ALL');
-//
-//	logger.trace('Entering cheese testing');
-//	logger.debug('Got cheese.');
-//	logger.info('Cheese is Gouda.');
-//	logger.warn('Cheese is quite smelly.');
-//	logger.error('Cheese is too ripe!');
-//	logger.fatal('Cheese was breeding ground for listeria.');
-//}());
+
--- test/logging.test.js
+++ test/logging.test.js
@@ -1,11 +1,11 @@
-/*jslint node:true, white:true, stupid: true */
+/*jslint node: true, white: true, stupid: true */
 "use strict";
 
 /*!
- * logging.test.js - Test logging functionality 
- * Copyright(c) 2011 Crafity
- * Copyright(c) 2012 Galina Slavova
- * Copyright(c) 2012 Bart Riemens
+ * crafity-logging - Test logging functionality 
+ * Copyright(c) 2013 Crafity
+ * Copyright(c) 2013 Bart Riemens
+ * Copyright(c) 2013 Galina Slavova
  * MIT Licensed
  */
 
@@ -13,61 +13,136 @@
  * Test dependencies.
  */
 
-console.log("Test is starting");
+var jstest = require('crafity-jstest').createContext("Crafity Logging Tests")
+	, fs = require('crafity-filesystem')
+	, assert = jstest.assert
+	, logging = require('../main.js')
+	;
 
-(function createLoggerWithoutAppender() {
+/**
+ * Run the tests
+ */
 
-	var logging = require('./../main')
-		, logger = logging.create();
+jstest.run({
 
-	logger.info("LOGGER:createLoggerWithoutParameters");
-	console.log("CONSOLE:createLoggerWithoutParameters");
+	"Require the crafity logging module": function () {
+		assert.isFunction(logging.create, "Expected logger to be a function.");
+	},
 
-}());
+	"When creating a logger without configuration Then a logger with default category is returned.": function () {
+		var logger = logging.create();
 
-(function createLoggerWithConsoleAppender() {
+		assert.isDefined(logger, "Expected logger to be defined.");
+		assert.isDefined(logger.category, "Expected a logger category to be defined.");
+		assert.areEqual("[default]", logger.category, "Expected this logger to have [default] category.");
+	},
 
-	var logging = require('./../main')
-		, logger = logging.create({
-			keep: false,
+	"When creating a logger without configuration Then a logger with undefined name is returned.": function () {
+		var logger = logging.create();
+
+		assert.isDefined(logger, "Expected logger to be defined.");
+		assert.isDefined(logger.name, "Expected a logger name to be defined.");
+	},
+
+	"When creating a logger without configuration Then a logger with no appenders is returned.": function () {
+		var logger = logging.create();
+
+		assert.isDefined(logger, "Expected logger to be defined.");
+		assert.areEqual(0, Object.keys(logger.appenders).length, "Expected this logger to have appenders.");
+	},
+
+	"When creating a logger without custom appenders but with flag to keep defaults Then has no appenders.": function () {
+		var logger = logging.create({ keep: false });
+
+		console.log("logger.appenders", logger.appenders);
+
+		assert.isDefined(logger, "Expected logger to be defined.");
+		assert.isDefined(logger.appenders, "Expected logger appenders list to be defined.");
+		assert.areEqual(0, Object.keys(logger.appenders).length, "Expected default appenders to be defined.");
+	},
+
+	"When configuring a logger with a file appender Then return a logger with one file appender.": function (test) {
+		test.async(2000);
+
+		// arrange
+		var path = "test/logs/test.log"
+			, messageToLog = "This test message is going to be logged to the file"
+			, logger = null
+			, logFileContents = null
+			;
+
+		// act
+
+		try {
+			fs.unlinkSync(path);
+		} catch (ignore) {
+
+		}
+
+		logger = logging.create({
 			appenders: [
 				{
-					type: "console",
-					name: "unittest"
-				}
-			]});
-
-	logger.info("LOGGER:createLoggerWithConsoleAppender");
-	console.log("CONSOLE:createLoggerWithConsoleAppender");
-
-}());
-
-(function createLoggerWithFileAppender() {
-
-	var logging = require('./../main')
-		, logger = logging.create({
-			keep: false,
-			appenders: [
-				{
-					type: "file",
-					name: "unittest",
-					filename: "test/logs/test.log",
-					console: true
+					type: "file", // input for lopg4js to choose from its appender list: console, file, etc
+					filename: path,
+					name: "unittest" // a specific name for the logger appender
 				}
 			]
 		});
 
-	logger.info("LOGGER:createLoggerWithFileAppender");
-	console.log("CONSOLE:createLoggerWithFileAppender");
+		console.log(messageToLog);
 
-}());
+		setTimeout(function () {
 
-(function createLoggerWithoutAppender() {
+			logFileContents = fs.readFileSync(path).toString();
 
-	var logging = require('../main')
-		, logger = logging.create({keep: false});
+			// assert
+			assert.isTrue(logFileContents.indexOf(messageToLog) > -1, "Expected this log file to have same content with the logged content.");
+			assert.areEqual("file", Object.keys(logger.appenders)[0], "Expected this logger to a file appender.");
 
-	logger.info("LOGGER:createLoggerWithoutAppender");
-	console.log("CONSOLE:createLoggerWithoutAppender");
+			console.log("WE ARE HERE...");
 
-}());
+			test.complete();
+		}, 10);
+
+	},
+
+	"When calling the http logger Then it will have method logRequestResponse.": function () {
+		// arrange + act
+		var logger = logging.create();
+
+		// assert
+		assert.isFunction(logger.http.logRequestResponse, "Expected the http logger to have function logRequestResponse");
+	},
+
+	"When calling the Http logger Then the ansi colour formatting will be used.": function () {
+		// arrange
+		var logger = logging.create()
+			, req = {
+				client: {
+					remoteAddress: "127.0.0.1",
+					remotePort: "50543"
+				},
+				method: "GET",
+				headers: {
+					host: "static.crafity.dev",
+					referer: "http://crafity.dev/",
+					"user-agent": "user-agent"
+				},
+				url: "/about/clients?layout=false"
+			}
+			, res = {
+				statusCode: "200"
+			}
+			;
+
+		// act
+		logger.http.logRequestResponse(req, res, null);
+
+		// assert
+		assert.isDefined(logger.http, "Expected the http logger to be defined.");
+		assert.isFunction(logger.http.logRequestResponse, "Expected the http logger to have function logRequestResponse");
+	}
+
+});
+
+
